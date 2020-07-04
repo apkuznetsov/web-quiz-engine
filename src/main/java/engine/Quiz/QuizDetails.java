5940e6219dff5d372ab11946b0a6bcdf5a09f879
Andrey <kouznts@ya.ru>
2020-07-04 19:13:42 +0400

    impl get quiz by id

--- src/main/java/engine/Quiz/QuizDetails.java
+++ src/main/java/engine/Quiz/QuizDetails.java
@@ -2,14 +2,27 @@ package engine.Quiz;
 
 public class QuizDetails {
 
+    final private int id;
     final private String title;
     final private String text;
     final private String[] options;
 
-    public QuizDetails(String title, String text, String[] options) {
+    public QuizDetails(int id, String title, String text, String[] options) {
+        this.id = id;
         this.title = title;
         this.text = text;
         this.options = options;
+    }
+
+    public QuizDetails(QuizEntry quiz) {
+        this.id = quiz.getId();
+        this.title = quiz.getTitle();
+        this.text = quiz.getText();
+        this.options = quiz.getOptions().clone();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
--- src/main/java/engine/QuizController.java
+++ src/main/java/engine/QuizController.java
@@ -1,13 +1,7 @@
 package engine;
 
-import engine.Quiz.Quiz;
-import engine.Quiz.QuizDetails;
-import engine.Quiz.QuizEntry;
-import engine.Quiz.QuizFeedback;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import engine.Quiz.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,15 +11,13 @@ public class QuizController {
 
     final private List<QuizEntry> db = new ArrayList<>();
 
-    final private String[] options = new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"};
-    final private QuizDetails quizDetails = new QuizDetails(
-            "The Java Logo",
-            "What is depicted on the Java logo?",
-            options);
-
-    @GetMapping(path = "/api/quiz")
-    public QuizDetails getQuizDetails() {
-        return quizDetails;
+    @GetMapping(path = "/api/quizzes/{id}")
+    public QuizDetails getQuiz(@PathVariable int id) {
+        try {
+            return new QuizDetails(db.get(id));
+        } catch (IndexOutOfBoundsException exc) {
+            throw new QuizNotFoundException();
+        }
     }
 
     @PostMapping(path = "/api/quiz")
