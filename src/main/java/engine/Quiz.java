eec75cae4407523a7675d705c9ec06632b13cd08
Andrey <kouznts@ya.ru>
2020-07-04 12:54:25 +0400

    impl quiz class

---
+++ src/main/java/engine/Quiz.java
@@ -0,0 +1,26 @@
+package engine;
+
+public class Quiz {
+
+    final private String title;
+    final private String text;
+    final private String[] options;
+
+    public Quiz(String title, String text, String[] options) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+}
