18f26366ddab4ca88369782ab0f3840f64ecab26
Andrey <kouznts@ya.ru>
2020-07-04 18:38:57 +0400

    impl add quiz post method

--- src/main/java/engine/QuizController.java
+++ src/main/java/engine/QuizController.java
@@ -1,13 +1,22 @@
 package engine;
 
+import engine.Quiz.Quiz;
 import engine.Quiz.QuizDetails;
+import engine.Quiz.QuizEntry;
 import engine.Quiz.QuizFeedback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class QuizController {
+
+    private int idSequence = 10;
+    final private List<QuizEntry> db = new ArrayList<>();
 
     final private String[] options = new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"};
     final private QuizDetails quizDetails = new QuizDetails(
@@ -25,5 +34,14 @@ public class QuizController {
         final boolean isSuccess = answer == 2;
 
         return new QuizFeedback(isSuccess, "Feedback!!!");
+    }
+
+    @PostMapping(path = "/api/quizzes")
+    public QuizEntry addQuiz(@RequestBody Quiz quiz) {
+        QuizEntry newQuizEntry = new QuizEntry(idSequence, quiz);
+        db.add(newQuizEntry);
+        idSequence++;
+
+        return newQuizEntry;
     }
 }
