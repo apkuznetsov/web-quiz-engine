cccdeac407174531b75636f57b97db7bd5401f2e
Andrey <kouznts@ya.ru>
2020-07-05 16:53:10 +0400

    add setters to quiz entry

--- src/main/java/engine/Quiz/QuizEntry.java
+++ src/main/java/engine/Quiz/QuizEntry.java
@@ -2,11 +2,11 @@ package engine.quiz;
 
 public class QuizEntry {
 
-    final private int id;
-    final private String title;
-    final private String text;
-    final private String[] options;
-    final private int[] answer;
+    private int id;
+    private String title;
+    private String text;
+    private String[] options;
+    private int[] answer;
 
     public QuizEntry(int id, String title, String text,
                      String[] options, int[] answer) {
@@ -29,19 +29,39 @@ public class QuizEntry {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getText() {
         return text;
     }
 
+    public void setText(String text) {
+        this.text = text;
+    }
+
     public String[] getOptions() {
         return options.clone();
     }
 
+    public void setOptions(String[] options) {
+        this.options = options.clone();
+    }
+
     public int[] getAnswer() {
         return answer.clone();
+    }
+
+    public void setAnswer(int[] answer) {
+        this.answer = answer.clone();
     }
 }
