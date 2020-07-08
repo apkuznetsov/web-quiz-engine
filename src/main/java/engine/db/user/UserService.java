ecee8d26fbf03324cccde7bd0f62f16917cb0e0f
Andrey <kouznts@ya.ru>
2020-07-08 14:10:21 +0400

    impl security config

---
+++ src/main/java/engine/db/user/UserService.java
@@ -0,0 +1,29 @@
+package engine.db.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        return user;
+    }
+}
