b56c69b9f6319003c80fe6d827ce499a607cbcb3
Andrey <kouznts@ya.ru>
2020-07-08 08:52:15 +0400

    fix controller's path

--- src/main/java/engine/QuizController.java
+++ src/main/java/engine/QuizController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class QuizController {
 
     @Autowired
@@ -19,7 +20,7 @@ public class QuizController {
     @Autowired
     private QuizRepository quizRepository;
     
-    @PostMapping(value = "/api/register", consumes = "application/json")
+    @PostMapping(value = "/register", consumes = "application/json")
     public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
         User foundUser = userRepository.findByEmail(user.getEmail());
         if (foundUser == null) {
@@ -30,13 +31,13 @@ public class QuizController {
         }
     }
 
-    @PostMapping(value = "/api/quizzes", consumes = "application/json")
+    @PostMapping(value = "/quizzes", consumes = "application/json")
     public ResponseEntity<Quiz> addQuiz(@Valid @RequestBody Quiz quiz) {
         quizRepository.save(quiz);
         return new ResponseEntity<>(quiz, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/api/quizzes/{id}")
+    @GetMapping(path = "/quizzes/{id}")
     public ResponseEntity<Quiz> getQuiz(@PathVariable Long id) {
         Quiz quiz = quizRepository.findById(id).orElse(null);
         return quiz == null
@@ -44,12 +45,12 @@ public class QuizController {
                 : new ResponseEntity<>(quiz, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/api/quizzes")
+    @GetMapping(path = "/quizzes")
     public ResponseEntity<List<Quiz>> getQuizzes() {
         return new ResponseEntity<>(new ArrayList<>(quizRepository.findAll()), HttpStatus.OK);
     }
 
-    @PostMapping(path = "/api/quizzes/{id}/solve")
+    @PostMapping(path = "/quizzes/{id}/solve")
     public ResponseEntity<QuizFeedback> solveQuiz(@PathVariable Long id, @RequestBody Answer answer) {
         Quiz quiz = quizRepository.findById(id).orElse(null);
         return quiz == null
